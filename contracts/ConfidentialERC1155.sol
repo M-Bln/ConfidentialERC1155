6d9c95484f21e79e79bd3c697ff92fe629ec14ec
M-Bln <mlnd2@protonmail.com>
2024-03-25 18:09:32 +0100

    A variant of ERC-1155 to associate confidential data to the token

---
+++ contracts/ConfidentialERC1155.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Define a user-defined type for additional data associated with each token
+struct TokenData {
+    uint256 tokenId;
+    euint32 x;
+}
+
+contract ConfidentialERC1155 is ERC1155, Ownable {
+    // Mapping from token ID to additional data
+    mapping(uint256 => TokenData) private _tokenData;
+
+    constructor(string memory uri) ERC1155(uri) {}
+
+    // Function to mint new tokens with additional data
+    function mintWithConfidentialData(
+        address account,
+        uint256 tokenId,
+        uint256 amount,
+        bytes calldata confidentialData,
+        bytes memory data
+    ) external onlyOwner {
+        super._mint(account, tokenId, amount, data);
+        _tokenData[tokenId] = TokenData(tokenId, TFHE.asEuint32(confidentialData));
+    }
+
+    // Function to get the additional data associated with a token
+    function getTokenData(uint256 tokenId) external view returns (TokenData memory) {
+        return _tokenData[tokenId];
+    }
+
+    // Override the _mint function to prevent direct minting without additional data
+    function _mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) internal override {
+        revert("Minting with additional data required");
+    }
+}
