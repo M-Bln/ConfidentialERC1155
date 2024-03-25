8a6f8b3c8adf00753e25ec026fce222f2cdc87ff
M-Bln <mlnd2@protonmail.com>
2024-03-25 18:09:32 +0100

    A variant of ERC-1155 to associate confidential data to the token

--- contracts/ConfidentialERC20.sol
+++ contracts/ConfidentialERC20.sol
@@ -2,9 +2,10 @@
 
 pragma solidity ^0.8.20;
 
-import "../../../../../../node_modules/fhevm/abstracts/Reencrypt.sol";
-import "../../../../../../node_modules/fhevm/lib/TFHE.sol";
-import "../../../../../../node_modules/@openzeppelin/contracts/access/Ownable2Step.sol";
+import "fhevm/abstracts/Reencrypt.sol";
+import "fhevm/lib/TFHE.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
 
 contract ConfidentialERC20 is Reencrypt, Ownable2Step {
     event Transfer(address indexed from, address indexed to);
