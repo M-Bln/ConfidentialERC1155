85b14a77e336d877a8f0b95883e7107531f552e3
M-Bln <mlnd2@protonmail.com>
2024-04-03 19:14:44 +0200

    change deploy.ts to deploy ConfidentialERC1155

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,3 +1,21 @@
+//import { DeployFunction } from "hardhat-deploy/types";
+//import { HardhatRuntimeEnvironment } from "hardhat/types";
+//
+//const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//  const { deployer } = await hre.getNamedAccounts();
+//  const { deploy } = hre.deployments;
+//
+//  const deployed = await deploy("ConfidentialERC20", {
+//    from: deployer,
+//    args: ["Naraggara", "NARA"],
+//    log: true,
+//  });
+//
+//  console.log(`ConfidentialERC20 contract: `, deployed.address);
+//};
+//export default func;
+//func.id = "deploy_confidentialERC20"; // id required to prevent reexecution
+//func.tags = ["ConfidentialERC20"];
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
@@ -5,14 +23,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployed = await deploy("ConfidentialERC20", {
+  const deployed = await deploy("ConfidentialERC1155", {
     from: deployer,
-    args: ["Naraggara", "NARA"],
+    args: ["Uri"],
     log: true,
   });
 
-  console.log(`ConfidentialERC20 contract: `, deployed.address);
+  console.log(`ConfidentialERC1155 contract: `, deployed.address);
 };
 export default func;
-func.id = "deploy_confidentialERC20"; // id required to prevent reexecution
-func.tags = ["ConfidentialERC20"];
+func.id = "deploy_confidentialERC1155"; // id required to prevent reexecution
+func.tags = ["ConfidentialERC1155"];
