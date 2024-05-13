675e227a92361569ed151951272fbe146e0d42fb
M-Bln <mlnd2@protonmail.com>
2024-05-13 11:46:56 +0200

    setup deploy script for ConfidentialErc1155

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,16 +5,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  // const deployedConfidentialERC1155 = await deploy("ConfidentialERC1155", {
-  //   from: deployer,
-  //   args: ["Uri"],
-  //   log: true,
-  // });
+  const deployedConfidentialERC1155 = await deploy("ConfidentialERC1155", {
+    from: deployer,
+    args: ["Uri"],
+    log: true,
+  });
 
-  // console.log(`ConfidentialERC1155 contract: `, deployedConfidentialERC1155.address);
+  console.log(`ConfidentialERC1155 contract: `, deployedConfidentialERC1155.address);
 
-  const deployedLightConfidentialToken = await deploy("LightConfidentialToken", { from: deployer, log: true });
-  console.log(`LightConfidentialToken contract: `, deployedLightConfidentialToken.address);
+  // const deployedLightConfidentialToken = await deploy("LightConfidentialToken", { from: deployer, log: true });
+  // console.log(`LightConfidentialToken contract: `, deployedLightConfidentialToken.address);
 
   // const deployedSimpleStorage = await deploy("SimpleStorage", { from: deployer, log: true });
   // console.log(`SimpleStorage contract: `, deployedSimpleStorage.address);
